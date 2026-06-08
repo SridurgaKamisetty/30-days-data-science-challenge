7b5839fb59b2cb8fcc023bddfae1c4cddc30c088
SridurgaKamisetty <kamisettysridurga@gmail.com>
2026-06-08 09:40:41 +0530

    Update day26fraud_detection.py

--- Day26_ML_Credit_Card_Fraud_Detection/day26fraud_detection.py
+++ Day26_ML_Credit_Card_Fraud_Detection/day26fraud_detection.py
@@ -28,7 +28,7 @@ y = df['Class']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y)
 
-model = LogisticRegression(max_iter=1000)
+model = LogisticRegression(class_weight='balanced')
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
